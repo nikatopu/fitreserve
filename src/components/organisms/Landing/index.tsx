716b9ebe5c40bf9bc0556283c5e8f0b89614cd45
gio.tsintsadze <tsintsadze-gio@mail.ru>
2026-06-22 14:09:35 +0400

    Feat/ Status open

--- src/components/organisms/Landing/index.tsx
+++ src/components/organisms/Landing/index.tsx
@@ -36,6 +36,27 @@ export default function Landing() {
           />
         </div>
       </div>
+
+      <div className={style.statusContainer}>
+        <svg
+          width="8"
+          height="8"
+          viewBox="0 0 8 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="8" height="8" rx="4" fill="var(--text-color-light)" />
+        </svg>
+
+        <Paragraph
+          text={
+            <>
+              STATION STATUS: <b>OPEN</b>
+            </>
+          }
+          size="small"
+        />
+      </div>
     </section>
   );
 }
